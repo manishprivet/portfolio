96aa52381c4ad632f63bd892413d2c02dee1917d
Manish Kumar <manishprivet@protonmail.com>
2020-08-06 11:22:31 +0530

    Added Icons

--- src/components/ProjectCard/ProjectCard.js
+++ src/components/ProjectCard/ProjectCard.js
@@ -1,34 +1,37 @@
 import React from 'react';
-import './ProjectCard.css'
+import './ProjectCard.css';
 import Tilt from 'react-tilt';
+import DevIcons from '../DevIcons/DevIcons';
 
-function ProjectCard({title,content,link,image,direction}) {
-    return(
-        <div className='main' style = {{flexDirection: direction}} >
-            <div className='description'>
-                <div className='cardText'>
-                    <h1>{title}</h1>
-                    <h3>{content}</h3>
-                    <a href={link} target='_blank' rel="noopener noreferrer">
-                        <button>See Live</button>
-                    </a>
-                </div>
-            </div>
-            <div className='image'>
-            <Tilt className="Tilt" options={{ max : 10, scale: 1 }}  >
-                <div className="Tilt-inner">
-                <div className='cardImageDiv'>
-                    <img 
-                        src={require(`../../images/${image}`) }
-                        alt=''
-                        className = 'cardImage'
-                    />
-                </div>
-                </div>
-            </Tilt>
-            </div>
+function ProjectCard({ title, content, link, image, direction, stack }) {
+  return (
+    <div className='main' style={{ flexDirection: direction }}>
+      <div className='description'>
+        <div className='cardText'>
+          <h1>{title}</h1>
+          <h3>{content}</h3>
+          {stack.map((icon) => <DevIcons icon={icon} />)}
+          <a href={link} target='_blank' rel='noopener noreferrer'>
+            <button>See Live</button>
+          </a>
         </div>
-    )
+      </div>
+      <div className='image'>
+        <Tilt className='Tilt' options={{ max: 10, scale: 1 }}>
+          <div className='Tilt-inner'>
+            <div className='cardImageDiv'>
+              <img
+                src={require(`../../images/${image}`)}
+                // src={`https://api.apiflash.com/v1/urltoimage?access_key=acefca5c960c4ad7a080fe271936a32c&url=${link}`}
+                alt=''
+                className='cardImage'
+              />
+            </div>
+          </div>
+        </Tilt>
+      </div>
+    </div>
+  );
 }
 
 export default ProjectCard;

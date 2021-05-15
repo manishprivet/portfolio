b4bf71db5ec28f4d7d3c40cea9c250c019a02827
Manish Kumar <manishprivet@protonmail.com>
2021-05-15 18:37:28 +0530

    Fixed Type Error

--- api/github-contributions.ts
+++ api/github-contributions.ts
@@ -4,7 +4,7 @@ import { fetchContributions } from "../utils/fetchers/contributions";
 const TOKEN = process.env.GITHUB_ACCESS_TOKEN;
 
 const handler = async (_: VercelRequest, res: VercelResponse) => {
-  const data = await fetchContributions("manishprivet", TOKEN);
+  const data = await fetchContributions("manishprivet", TOKEN || "");
   res.status(200).json(data);
 };
 
